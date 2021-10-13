3db9f83292a3015ffa2b3a3f83b2fcffe0cd4026
gbelda <gbelda56@gmail.com>
2021-10-13 16:37:00 +0200

    Added JS elements
    aggiunto i variabili per ciascun multipli

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,14 +1,22 @@
-// assegnare i variabili agli elementi del HTML
-const numberContainer = document.querySelector('.numbers')
+// individuare e assegnare i variabili da utilizzare
+const numberContainer = document.querySelector('.numbers');
+const fizzElement = `<li class="box fizz">Fizz</li>`;
+const buzzElement = `<li class="box buzz">Buzz</li>`;
+const fizzBuzzElement = `<li class="box fizzbuzz">FizzBuzz</li>`;
 
 //creare un ciclo di numeri da 1 a 100
+
+
 for (let index = 1; index <= 100; index++) {
     //appendere per ogni iterazione di numero un container e riportarlo al HTML
-    const boxElement = `<li class=box>${index}</li>`;
-    numberContainer.insertAdjacentHTML("beforeend", boxElement)
+    const boxMarkup = `<li class="box ">${index}</li>`;
+    numberContainer.insertAdjacentHTML("beforeend", boxMarkup)
+
 
 
 }
+
+
 //stabilire i multipli di 3
 //stabilire i multipli di 5
 //stabilire i multipli sia di 3 e sia di 5
