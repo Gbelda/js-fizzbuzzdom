2304dadfdc2a1fa6ac7b4aebae74f6cdc40affce
gbelda <gbelda56@gmail.com>
2021-10-13 17:21:37 +0200

    Complete excercise
    Applicato il condizionale per i multipli di 3 e 5 nel loop e sistemato lo styling

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,18 +5,34 @@ const buzzElement = `<li class="box buzz">Buzz</li>`;
 const fizzBuzzElement = `<li class="box fizzbuzz">FizzBuzz</li>`;
 
 //creare un ciclo di numeri da 1 a 100
-
-
 for (let index = 1; index <= 100; index++) {
     //appendere per ogni iterazione di numero un container e riportarlo al HTML
-    const boxMarkup = `<li class="box ">${index}</li>`;
-    numberContainer.insertAdjacentHTML("beforeend", boxMarkup)
+    const neutralElement = `<li class="box neutral">${index}</li>`;
+
+    //stabilire i multipli sia di 3 e sia di 5
+    if (index % 3 == 0 && index % 5 == 0) {
+        numberContainer.insertAdjacentHTML("beforeend", fizzBuzzElement)
+
+        //stabilire i multipli di 5
+    } else if (index % 5 == 0) {
+        numberContainer.insertAdjacentHTML("beforeend", buzzElement)
+
+        //stabilire i multipli di 3
+    } else if (index % 3 == 0) {
+
+        numberContainer.insertAdjacentHTML("beforeend", fizzElement)
+
+    } else {
+        numberContainer.insertAdjacentHTML("beforeend", neutralElement)
+    }
+
 
 
 
 }
 
 
-//stabilire i multipli di 3
-//stabilire i multipli di 5
-//stabilire i multipli sia di 3 e sia di 5
+
+
+
+
